e1cadb0d5ebbaae0d745753b9c7644bd3dcdbe94
Clement Joye <clement.joye@authority.se>
2024-03-23 23:42:46 +0100

    First commit.

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
-# rsakey
+# Description
 Encrypt with private RSA key and decrypt with public key
+
+# Generate RSA key
+
+Open a terminal :  
+> openssl genrsa -out pem_private.pem 1024  
+> openssl rsa -in pem_private.pem -pubout > pem_public.pem
+
+If openssl is not installed on your machine, use the one that comes with Git `C:\Program Files\Git\usr\bin>`
---
+++ RsaKey/Program.cs
@@ -0,0 +1,78 @@
+﻿using Org.BouncyCastle.Crypto.Encodings;
+using Org.BouncyCastle.Crypto.Engines;
+using Org.BouncyCastle.Crypto.Parameters;
+using Org.BouncyCastle.OpenSsl;
+using System.Text;
+
+namespace RsaKey
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string plainText = "localhost";
+
+            string cipherText = Encrypt(plainText);
+            string decryptedCipherText = Decrypt(cipherText);
+
+            Console.WriteLine("Encrypted text: {0}", cipherText);
+            Console.WriteLine("Decrypted text {0}. Encryption/Decryption was correct {1}",
+                decryptedCipherText, (plainText == decryptedCipherText).ToString());
+        }
+
+        static string Decrypt(string cipherText)
+        {
+            byte[] cipherTextBytes = Convert.FromBase64String(cipherText);
+
+            PemReader pr = new(
+                File.OpenText("./pem_public.pem")
+            );
+            RsaKeyParameters keys = (RsaKeyParameters)pr.ReadObject();
+
+            OaepEncoding eng = new(new RsaEngine());
+            eng.Init(false, keys);
+
+            int length = cipherTextBytes.Length;
+            int blockSize = eng.GetInputBlockSize();
+            List<byte> plainTextBytes = [];
+            for (int chunkPosition = 0;
+                chunkPosition < length;
+                chunkPosition += blockSize)
+            {
+                int chunkSize = Math.Min(blockSize, length - chunkPosition);
+                plainTextBytes.AddRange(eng.ProcessBlock(
+                    cipherTextBytes, chunkPosition, chunkSize
+                ));
+            }
+            return Encoding.UTF8.GetString(plainTextBytes.ToArray());
+        }
+
+        static string Encrypt(string plainText)
+        {
+            byte[] plainTextBytes = Encoding.UTF8.GetBytes(plainText);
+
+            PemReader pr = new(
+                File.OpenText("./pem_private.pem")
+            );
+
+            var key = (RsaPrivateCrtKeyParameters)pr.ReadObject();
+
+            OaepEncoding eng = new(new RsaEngine());
+            eng.Init(true, key);
+
+            int length = plainTextBytes.Length;
+            int blockSize = eng.GetInputBlockSize();
+            List<byte> cypherTextBytes = [];
+            for (int chunkPosition = 0;
+                chunkPosition < length;
+                chunkPosition += blockSize)
+            {
+                int chunkSize = Math.Min(blockSize, length - chunkPosition);
+                cypherTextBytes.AddRange(eng.ProcessBlock(
+                    plainTextBytes, chunkPosition, chunkSize
+                ));
+            }
+            return Convert.ToBase64String(cypherTextBytes.ToArray());
+        }
+    }
+}
